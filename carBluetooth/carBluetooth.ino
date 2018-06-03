f10dd6f1deb4363e01f0cfe3cafdc6676dd7f610
SimoDimitrov <simo.dimitrov.inc@gmail.com>
2018-06-03 22:36:13 +0300

    Add code

---
+++ carBluetooth/carBluetooth.ino
@@ -0,0 +1,100 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(11, 12); // RX | TX
+#include <Servo.h>
+
+const int forward = 2; // julto
+const int backward = 4; // cerveno
+const int left = 6; // zeleno
+const int right = 8; // cerno
+
+char in, state_m1 = 's', state_m2 = 'v';
+
+long time;
+
+Servo servo;
+
+int pos = 0;
+
+void setup() 
+{
+
+    Serial.begin(9600);
+
+    pinMode(forward, OUTPUT);
+    pinMode(backward, OUTPUT);
+    pinMode(left, OUTPUT);
+    pinMode(right, OUTPUT);
+ 
+    BTserial.begin(9600);
+
+    servo.attach(9);
+    
+//    for (pos = 0; pos <= 180; pos += 1) {
+//      servo.write(pos);
+//      delay(10);
+//    }
+//    for (pos = 90; pos >= 0; pos -= 1) {
+//      servo.write(pos);
+//      delay(10);
+//    }
+//  
+}
+ 
+void loop()
+{
+//        if (BTserial.available() > 0)
+//        {
+
+            Serial.print("Time: ");
+            time = millis();
+            Serial.print(time); 
+            Serial.print(" ");
+            
+            in = BTserial.read();
+            
+            Serial.println(in);
+
+            if(in == 'f')
+            {
+              digitalWrite(backward, LOW);
+              digitalWrite(forward, HIGH);
+            }
+
+            if(in == 'b')
+            {
+              digitalWrite(forward, LOW);
+              digitalWrite(backward, HIGH);
+
+            Serial.print("Timeeeeeeeees: ");
+            time = millis();
+            Serial.println(time);
+              
+            }
+
+            if(in == 's')
+            {
+              digitalWrite(backward, LOW);
+              digitalWrite(forward, LOW);
+            }
+
+
+
+            if(in == 'l')
+            {
+              digitalWrite(right, LOW);
+              digitalWrite(left, HIGH);
+            }
+
+            if(in == 'r')
+            {
+              digitalWrite(left, LOW);
+              digitalWrite(right, HIGH);
+            }
+
+            if(in == 'v')
+            {
+              digitalWrite(left, LOW);
+              digitalWrite(right, LOW);
+            }
+//         }
+}
---
+++ distancionno_djoystik/distancionno_djoystik.ino
@@ -0,0 +1,43 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(3, 2);
+
+long state_x, state_y;
+int b1, b2;
+ 
+void setup() 
+{
+    Serial.begin(9600);
+    BTserial.begin(9600);
+    pinMode(8, INPUT_PULLUP);
+    pinMode(9, INPUT_PULLUP);
+}
+ 
+void loop()
+{
+
+
+    state_x = analogRead(A0);
+    state_y = analogRead(A1);
+    b1 = digitalRead(8);
+    b2 = digitalRead(9);
+
+     if(state_x < 400){
+        Serial.println("f");
+        BTserial.write("f");
+      }else if(state_x > 600){
+        Serial.println("b");
+        BTserial.write("b");
+      }else BTserial.write("s");
+
+
+      if(state_y < 400){
+        Serial.println("r");
+        BTserial.write("r");
+      }else if(state_y > 600){
+        Serial.println("l");
+        BTserial.write("l");
+      }else BTserial.write("v");
+
+      Serial.println(b1);
+      Serial.println(b2);
+}
